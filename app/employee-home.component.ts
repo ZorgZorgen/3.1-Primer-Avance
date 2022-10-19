4b5211dc7f55b47602d25c0827623005aa31cb36
zorgzorgen <wriedel13@gmail.com>
2022-10-18 18:09:06 -0700

    Add files via upload

---
+++ app/employee-home.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'employee-home',
+  template:'<h2>Sistema de Administracion de Empleados</h2>',
+  styles:[`
+      h2{
+        text-align:center
+      }`
+  ]
+  })
+export class EmployeeHomeComponent{}
